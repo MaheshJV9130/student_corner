2abbf56011e2d39b362a95b3af95bd30cfcfa5b4
Mahesh Vispute <maheshvispute082@gmail.com>
2025-10-07 22:02:29 +0530

    db

--- backend/lib/database.js
+++ backend/lib/database.js
@@ -2,7 +2,7 @@ import mongoose, { connect } from "mongoose";
 
 export const connect_database = async() => {
   try {
-    await connect(process.env.DB_URL)
+    await connect("mongodb+srv://maheshVispute:maheshVispute@roomkart.b2hdd0a.mongodb.net/")
     console.log("DB Connected")
   } catch (error) {
     console.log("Failed to connect with DB")
